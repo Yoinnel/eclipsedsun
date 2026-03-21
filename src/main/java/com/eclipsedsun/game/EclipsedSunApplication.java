174233c8385ef514966008b96d0be2b2a775923f
Yoinnel Gabriel Martinez Brito <britogabriel082@gmail.com>
2026-03-20 19:31:36 -0500

    Cambio de tamaño de pantalla de inicio

--- src/main/java/com/eclipsedsun/game/EclipsedSunApplication.java
+++ src/main/java/com/eclipsedsun/game/EclipsedSunApplication.java
@@ -11,7 +11,7 @@ public class EclipsedSunApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(EclipsedSunApplication.class.getResource("start-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Eclipsed Sun");
         stage.setScene(scene);
         stage.show();
